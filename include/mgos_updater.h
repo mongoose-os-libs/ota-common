7e83a02f857340047fc77e6bd784473881ea9d3a
Deomid Ryabkov <rojer@cesanta.com>
2018-11-29 13:39:33 +0000

    Move things around a bit in the OTA libs
    Put away the internals, only expose what's needed for interaction with external libs.
    
    CL: none
    
    PUBLISHED_FROM=1499409859657b0259335ace9fb86258fa68e5b0

--- include/mgos_updater.h
+++ include/mgos_updater.h
@@ -17,9 +17,18 @@
 
 #include "frozen.h"
 
+#include "mgos_event.h"
+
 #ifdef __cplusplus
 extern "C" {
-#endif /* __cplusplus */
+#endif
+
+#define MGOS_EVENT_OTA_BASE MGOS_EVENT_BASE('O', 'T', 'A')
+enum mgos_event_ota {
+  MGOS_EVENT_OTA_BEGIN =
+      MGOS_EVENT_OTA_BASE, /* ev_data: struct mgos_upd_info */
+  MGOS_EVENT_OTA_STATUS,   /* ev_data: struct mgos_ota_status */
+};
 
 struct mgos_upd_file_info {
   char name[50];
@@ -40,8 +49,38 @@ struct mgos_upd_info {
   struct mgos_upd_file_info current_file;
 };
 
+enum mgos_ota_state {
+  MGOS_OTA_STATE_IDLE = 0, /* idle */
+  MGOS_OTA_STATE_PROGRESS, /* "progress" */
+  MGOS_OTA_STATE_ERROR,    /* "error" */
+  MGOS_OTA_STATE_SUCCESS,  /* "success" */
+};
+
+struct mgos_ota_status {
+  bool is_committed;
+  int commit_timeout;
+  int partition;
+  enum mgos_ota_state state;
+  const char *msg;      /* stringified state */
+  int progress_percent; /* valid only for "progress" state */
+};
+
+const char *mgos_ota_state_str(enum mgos_ota_state state);
+
+void mgos_upd_boot_finish(bool is_successful, bool is_first);
+bool mgos_upd_commit();
+bool mgos_upd_is_committed();
+bool mgos_upd_is_first_boot(void);
+bool mgos_upd_revert(bool reboot);
+bool mgos_upd_get_status(struct mgos_ota_status *);
+/* Apply update on first boot, usually involves merging filesystem. */
+int mgos_upd_apply_update(void);
+
+int mgos_upd_get_commit_timeout();
+bool mgos_upd_set_commit_timeout(int commit_timeout);
+
 #ifdef __cplusplus
 }
-#endif /* __cplusplus */
+#endif
 
 #endif /* CS_FW_SRC_MGOS_UPDATER_H_ */
